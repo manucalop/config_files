e6517a18809ae0b793978b8b18c074b89ccc7bec
Manuel Castillo <manuelcloop@tutamail.com>
2018-09-26 18:28:52 +0200

    Fixed bug in initial_config

--- .config/i3/scripts/initial_config.bash
+++ .config/i3/scripts/initial_config.bash
@@ -11,7 +11,10 @@
     export XDG_CURRENT_DESKTOP=XFCE
     xfsettingsd &
 # Keyboard Default language, pro mode
-    setxkbmap -layout us; mode "default"
-    setxkbmap -option && setxkbmap -option ctrl:nocaps && xcape -e 'Control_L=Escape'; mode "default"
+    setxkbmap -layout us
+    setxkbmap -option && setxkbmap -option ctrl:nocaps && xcape -e 'Control_L=Escape'
 # Set local time as system time
 #    timedatectl set-local-rtc 1
+
+# For the settings
+    export XDG_CURRENT_DESKTOP=XFCE
