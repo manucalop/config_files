64ef4fa222c03dfcbf0d0f5e170c7acc83798303
Manuel Castillo <manucalop@gmail.com>
2019-09-11 18:05:03 +0200

    py3status scripts

---
+++ .config/i3/py3status/my_spotify.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import subprocess
+import os
+class Py3status:
+    format = 'Settings'
+
+    def __init__(self):
+        self.full_text = self.format
+
+    def click_info(self):
+        return {
+            'full_text': self.format,
+            'cached_until': self.py3.CACHE_FOREVER
+        }
+
+    def on_click(self, event):
+        with open(os.devnull, 'wb') as devnull:
+            subprocess.check_call('spotify', 
+                                   stdout=devnull, 
+                                   stderr=subprocess.STDOUT)
