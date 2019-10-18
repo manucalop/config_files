80b7186464b933529f3b439df07354eb806ec7e4
Manuel Castillo <manucalop@gmail.com>
2019-10-18 11:47:17 +0200

    Lockscreen fixed

---
+++ .config/i3/py3status/my_bar_button.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import subprocess
+import os
+class Py3status:
+    format = 'My Button'
+    cmd = 'thunar'
+
+    def __init__(self):
+        self.full_text = self.format
+
+    def click_info(self):
+        return {
+            'full_text': self.format,
+            'cached_until': self.py3.CACHE_FOREVER
+        }
+
+    def on_click(self, event):
+        with open(os.devnull, 'wb') as devnull:
+            subprocess.run(cmd, stdout=devnull, stderr=subprocess.STDOUT, shell=True )
