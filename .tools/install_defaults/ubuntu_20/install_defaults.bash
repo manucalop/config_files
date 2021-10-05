616c0f9d82b0a18ea4810c566a6f2eb9468b4877
Manuel Castillo <manucalop@gmail.com>
2021-10-05 11:08:48 +0200

    Modified install_defaults

--- .tools/install_defaults/ubuntu_20/install_defaults.bash
+++ .tools/install_defaults/ubuntu_20/install_defaults.bash
@@ -1,5 +1,8 @@
 #! /bin/bash
-sudo python3 install_defaults_apt.py && python3 install_defaults_pip.py
+sudo python3 install_defaults_apt.py 
+#python3 install_defaults_pip.py
 
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/denysdovhan/gnome-terminal-one/master/one-dark.sh)"
