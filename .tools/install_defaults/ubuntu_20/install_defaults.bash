88eb6ca689b666fef902a1fead246a4750ddd41e
Manuel Castillo <manucalop@gmail.com>
2021-10-05 12:00:06 +0200

    Removed nodejs from default

--- .tools/install_defaults/ubuntu_20/install_defaults.bash
+++ .tools/install_defaults/ubuntu_20/install_defaults.bash
@@ -2,7 +2,7 @@
 sudo python3 install_defaults_apt.py 
 #python3 install_defaults_pip.py
 
-curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs
+# curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+# sudo apt-get install -y nodejs
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/denysdovhan/gnome-terminal-one/master/one-dark.sh)"
