738e3cffc8590b021c98e7a31bd245086f73b8f5
Manuel Castillo <manucalop@gmail.com>
2022-02-17 09:57:46 +0100

    Updated py3status conf

--- .config/i3/py3status/my_file_manager.py
+++ .config/i3/py3status/my_file_manager.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python3
 import subprocess
 import os
+
+
 class Py3status:
-    format = 'Files'
+    format = "Files"
 
     def __init__(self):
         self.full_text = self.format
 
     def click_info(self):
-        return {
-            'full_text': self.format,
-            'cached_until': self.py3.CACHE_FOREVER
-        }
+        return {"full_text": self.format, "cached_until": self.py3.CACHE_FOREVER}
 
     def on_click(self, event):
-        with open(os.devnull, 'wb') as devnull:
-            subprocess.check_call('thunar', 
-                                   stdout=devnull, 
-                                   stderr=subprocess.STDOUT)
+        with open(os.devnull, "wb") as devnull:
+            subprocess.check_call("nautilus", stdout=devnull, stderr=subprocess.STDOUT)
--- .config/i3/py3status/my_settings.py
+++ .config/i3/py3status/my_settings.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 import subprocess
 import os
+
+
 class Py3status:
-    format = 'Settings'
+    format = "Settings"
 
     def __init__(self):
         self.full_text = self.format
 
     def click_info(self):
-        return {
-            'full_text': self.format,
-            'cached_until': self.py3.CACHE_FOREVER
-        }
+        return {"full_text": self.format, "cached_until": self.py3.CACHE_FOREVER}
 
     def on_click(self, event):
-        with open(os.devnull, 'wb') as devnull:
-            subprocess.check_call('xfce4-settings-manager', 
-                                   stdout=devnull, 
-                                   stderr=subprocess.STDOUT)
+        with open(os.devnull, "wb") as devnull:
+            subprocess.check_call(
+                "gnome-control-center", stdout=devnull, stderr=subprocess.STDOUT
+            )
