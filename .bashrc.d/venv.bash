624be3b3c443744b2a8185593af1e815d94c5e67
Manuel Castillo <manucalop@gmail.com>
2022-02-26 15:35:23 +0100

    Removed deprecated venv

--- .bashrc.d/venv.bash
+++ .bashrc.d/venv.bash
@@ -22,39 +22,3 @@ function venv_deactivate(){
 function venv_make(){
     python3 -m venv ./venv
 }
-
-#######################
-# Deprecated
-#######################
-
-# function venv_load(){
-# env="$1"
-# if [ -z "$1" ]
-# then
-#     echo "No environment selected these are available:"
-#     ls ~/.venvs/
-# else
-#     source ~/.venvs/$env/bin/activate
-# fi
-# }
-
-# function venv_create(){
-# env="$1"
-# if [ -z "$1" ]
-# then
-#     echo "No environment name provided"
-# else
-#     python3 -m venv ~/.venvs/$env
-#     source ~/.venvs/$env/bin/activate
-# fi
-# }
-
-# function venv_destroy(){
-# env="$1"
-# if [ -z "$1" ]
-# then
-#     echo "No environment name provided"
-# else
-#     rm -rf ~/.venvs/$env
-# fi
-# }
