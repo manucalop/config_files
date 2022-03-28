4dd3eacb638de21363d7e1d8cf73d3c58545730d
Manuel Castillo <manucalop@gmail.com>
2022-03-28 10:19:00 +0200

    Added copilot to lvim

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -3,9 +3,28 @@
 -- Plugins{{{
 lvim.plugins = {
     {"folke/tokyonight.nvim"},
+    {"github/copilot.vim"},
     {"folke/trouble.nvim", cmd = "TroubleToggle"},
 }
 --}}}
+
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_assume_mapped = true
+vim.g.copilot_tab_fallback = ""
+local cmp = require "cmp"
+
+lvim.builtin.cmp.mapping["<Tab>"] = function(fallback)
+  if cmp.visible() then
+    cmp.select_next_item()
+  else
+    local copilot_keys = vim.fn["copilot#Accept"]()
+    if copilot_keys ~= "" then
+      vim.api.nvim_feedkeys(copilot_keys, "i", true)
+    else
+      fallback()
+    end
+  end
+end
 
 -- Key mappings {{{
 
