0c079c0ee0e599b4b7ba374be2030975e38cc789
Manuel Castillo <manucalop@gmail.com>
2022-06-04 22:31:41 +0200

    Updating .bashrc.d

--- .bashrc.d/gcp.bash
+++ .bashrc.d/gcp.bash
@@ -1,3 +1,9 @@
 #!/usr/bin/bash
 export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/.config/gcloud/application_default_credentials.json"
 
+function slu_airbyte() {
+    gcloud compute ssh \
+        --zone "europe-west1-b" "airbyte" \
+        --project "prj-gcp-sls-sl01-poc01-lsc01" \
+        -- -L 8000:localhost:8000 -N -f
+}
