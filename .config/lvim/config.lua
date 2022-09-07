e1d637cbb87694eb72f89b75400dd133bd5597a6
Manuel Castillo <manucalop@gmail.com>
2022-09-07 12:24:34 +0200

    lvim updates

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -171,19 +171,19 @@ vim.cmd("au BufWritePre * :silent! Neoformat")
 -- Linters {{{
 local linters = require "lvim.lsp.null-ls.linters"
 linters.setup {
-    {
-        command = "flake8",
-        filetypes = { "python" },
-        extra_args = { "--max-line-length=10000" },
-    },
-    {
-        command = "shellcheck",
-        extra_args = { "--severity", "warning" },
-    },
-    {
-        command = "codespell",
-        filetypes = { "javascript", "python" },
-        -- extra_args = { "--print-with=100", "--line-width=100" },
-    },
+    -- {
+    --     command = "flake8",
+    --     filetypes = { "python" },
+    --     extra_args = { "--max-line-length=10000" },
+    -- },
+    -- {
+    --     command = "shellcheck",
+    --     extra_args = { "--severity", "warning" },
+    -- },
+    -- {
+    --     command = "codespell",
+    --     filetypes = { "javascript", "python" },
+    --     -- extra_args = { "--print-with=100", "--line-width=100" },
+    -- },
 }
 --}}}
