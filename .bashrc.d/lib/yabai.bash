3adc7304f2bb80b1e3dd84bbc5f1df62e96934b1
Manuel Castillo <manucalop@gmail.com>
2022-09-20 12:22:11 +0200

    Small changes

--- .bashrc.d/lib/yabai.bash
+++ .bashrc.d/lib/yabai.bash
@@ -1,8 +1,10 @@
-function yabai-init(){
-    sudo yabai --install-sa
-    sudo yabai --load-sa
-}
 
 function yabai-restart(){
     brew services restart yabai
+    sudo yabai --load-sa
+}
+
+function yabai-init(){
+    sudo yabai --install-sa
+    yabai-restart
 }
