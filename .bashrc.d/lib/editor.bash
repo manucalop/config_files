75fac38ee72557885fdea42e5b2c56c07849ef0a
Manuel Castillo-Lopez <manucalop@gmail.com>
2022-12-28 09:43:33 +0100

    Improved editor management

--- .bashrc.d/lib/editor.bash
+++ .bashrc.d/lib/editor.bash
@@ -1,11 +1,10 @@
 #!/usr/bin/bash
-export EDITOR='lvim'
-export VISUAL="$EDITOR"
+# export EDITOR='lvim'
+# export VISUAL="$EDITOR"
 
-alias vim="$EDITOR"
-alias vimcd='cd ~/.config/lvim'
-alias vimconf='vim ~/.config/lvim/lua/manucalop/'
-# alias vimconf='vim ~/.config/nvim/init.vim'
+# alias ed="$EDITOR"
+# alias vimcd='cd ~/.config/lvim'
+# alias vimconf='${EDITOR} ~/.config/lvim/lua/manucalop/'
 
 vi_mode(){
   TEST=`bind -v | awk '/keymap/ {print $NF}'`
@@ -15,3 +14,26 @@ vi_mode(){
      echo -ne "\033]12;Red\007"
   fi
 }
+
+set_editor(){
+  if [ -x "$(command -v lvim)" ]; then
+    export EDITOR='lvim'
+    export VISUAL="$EDITOR"
+    alias vimcd='cd ~/.config/lvim'
+    alias vimconf='${EDITOR} ~/.config/lvim/lua/manucalop/'
+  elif [ -x "$(command -v nvim)" ]; then
+    export EDITOR='nvim'
+    export VISUAL="$EDITOR"
+    alias vimcd='cd ~/.config/nvim'
+    alias vimconf='${EDITOR} ~/.config/nvim/lua/manucalop/'
+  elif [ -x "$(command -v vim)" ]; then
+    export EDITOR='vim'
+    export VISUAL="$EDITOR"
+    alias vimcd='cd ~/.vim'
+    alias vimconf='${EDITOR} ~/.vim/lua/manucalop/'
+  fi
+
+  alias vim="$EDITOR"
+}
+
+set_editor
