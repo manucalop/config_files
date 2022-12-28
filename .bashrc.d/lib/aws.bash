11e6c5f04159e8b7815f05efef5861542a0874bd
Manuel Castillo-Lopez <manucalop@gmail.com>
2022-12-28 09:18:18 +0100

    Tracking scripts

--- .bashrc.d/lib/aws.bash
+++ .bashrc.d/lib/aws.bash
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+export AWS_PROFILE=staging
+
 function ec2sync() {
     target=$1
     foldername=$(basename "$PWD")
+    root_folder=$(whoami)
     echo "Syncing $foldername to EC2";
     cd ..;
-    # Exclude $foldername/env folder recursively
-    rsync -azP --exclude .venv/ $foldername/ $target:~/$foldername;
+    # Create folder whoami folder on EC2
+    ssh $target 'mkdir -p ~/manucalop';
+    rsync -azP --exclude .venv/ $foldername/ $target:~/$root_folder/$foldername;
     cd $foldername;
 }
 
