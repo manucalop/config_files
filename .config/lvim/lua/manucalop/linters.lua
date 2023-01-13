ba15032e8d2be01463e7be7a6cc72555ddbdfce4
Manuel Castillo-Lopez <manucalop@gmail.com>
2023-01-13 12:17:52 +0100

    Linter config

--- .config/lvim/lua/manucalop/linters.lua
+++ .config/lvim/lua/manucalop/linters.lua
@@ -1,11 +1,18 @@
 local linters = require "lvim.lsp.null-ls.linters"
+
+local function get_git_root_dir()
+  local git_dir = vim.fn.system("git rev-parse --show-toplevel")
+  return git_dir:gsub("%s+", "")
+end
+
 linters.setup {
   {
     command = "flake8",
     filetypes = { "python" },
-    extra_args = { "--max-line-length=80" },
+    extra_args = {
+      "--config=" .. get_git_root_dir() .. "/setup.cfg",
+    },
   },
-
   -- {
   --     command = "shellcheck",
   --     extra_args = { "--severity", "warning" },
