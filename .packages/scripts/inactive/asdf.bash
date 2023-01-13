a8a6b125227c48cbac1259f7aa6aab69422352e3
Manuel Castillo-Lopez <manucalop@gmail.com>
2023-01-13 13:00:40 +0100

    Added default packages

---
+++ .packages/scripts/inactive/asdf.bash
@@ -0,0 +1,2 @@
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.00
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
--- setup.bash
+++ setup.bash
@@ -1,9 +1,20 @@
 #!/bin/bash
 # Setup config files
+#
+function setup_config_files {
+    echo "Setting up config files..."
+    git clone git@github.com:manucalop/config_files.git
+    cp -r config_files/. ~/
+    rm -rf config_files
+    cp -r config/* ~/.config/
+}
 
-git clone git@github.com:manucalop/config_files.git
-
-# Coping all files to home directory
-cp -r config_files/. ~/
-
-rm -rf config_files
+function install_packages {
+    echo "Installing packages..."
+    xargs -d '\n' -- sudo apt install -y < ./.packages/apt_pkgs.txt
+    for file in ./packages/scripts/*.bash; do
+        ./$file
+    done
+    # xargs -d '\n' -- pip install < ./packages/pip_pkgs.txt
+    # xargs -d '\n' -- sudo npm install -g < ./packages/npm_pkgs.txt
+}
