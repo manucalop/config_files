c88220ba63feaa085079d441337f17c9a9e78035
Manuel Castillo-Lopez <manucalop@gmail.com>
2023-11-22 17:50:47 +0100

    Load bash completions only if bash

--- .bashrc.d/lib/brew.bash
+++ .bashrc.d/lib/brew.bash
@@ -1,7 +1,9 @@
 # Bash completions
 export BREW_HOME=$(brew --prefix)
 
-for file in $BREW_HOME/etc/bash_completion.d/*
-do
+# If shell is bash, then load bash completions
+if [ -n "$BASH_VERSION" ]; then
+  for file in $BREW_HOME/etc/bash_completion.d/*; do
     source $file
-done
+  done
+fi
