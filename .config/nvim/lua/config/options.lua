397359e13e202d6850173d6d3af8ad4978514da9
Manuel Castillo-Lopez <manucalop@gmail.com>
2024-04-03 16:27:32 +0200

    Tracking nvim changes

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,3 +6,11 @@ local brew_dir = vim.fn.system("brew --prefix"):gsub("\n", "")
 
 vim.g.python3_host_prog = brew_dir .. "/bin/python3"
 vim.g.python3_host_prog_pip = brew_dir .. "/bin/pip3"
+
+-- LazyVim root dir detection
+-- Each entry can be:
+-- * the name of a detector function like `lsp` or `cwd`
+-- * a pattern or array of patterns like `.git` or `lua`.
+-- * a function with signature `function(buf) -> string|string[]`
+-- vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
+vim.g.root_spec = { "cwd" }
--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,4 +1,19 @@
 return {
-  { "alexghergh/nvim-tmux-navigation" },
   { "nvim-notify", enabled = true },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-w>h", "<cmd>TmuxNavigateLeft<cr>" },
+      { "<c-w>j", "<cmd>TmuxNavigateDown<cr>" },
+      { "<c-w>k", "<cmd>TmuxNavigateUp<cr>" },
+      { "<c-w>l", "<cmd>TmuxNavigateRight<cr>" },
+    },
+  },
 }
