8d411b2083ba5d2e23443d63bf3293b0f77a8eea
Manuel Castillo-Lopez <manucalop@gmail.com>
2024-12-05 19:30:10 +0100

    Tracking nvim settings

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,3 +2,4 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.cmd("au BufEnter *.tex setlocal wrap")
+vim.cmd("au BufEnter *.md setlocal nowrap")
