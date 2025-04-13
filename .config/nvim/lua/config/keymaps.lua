84bd9694a6127097c20ab6c369c46c50ed84bdcd
Manuel Castillo-Lopez <manucalop@gmail.com>
2025-04-13 12:57:35 +0200

    Changed buffer shortcuts

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,23 +3,20 @@
 -- Add any additional keymaps here
 local map = vim.keymap.set
 
-map({ "n" }, "<C-c>", "<cmd>q<CR>", { desc = "Quit" })
-map({ "n" }, "<C-n>", "<cmd>bnext<CR>", { desc = "Next Buffer" })
-map({ "n" }, "<C-p>", "<cmd>bprev<CR>", { desc = "Previous Buffer" })
+map("n", "<C-c>", "<cmd>q<CR>", { desc = "Quit" })
+map("n", "K", "O<Esc>j", { desc = "Insert newline above" })
+map("n", "J", "o<Esc>k", { desc = "Insert newline below" })
+map("n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", { desc = "Hover" })
+map("n", "gH", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { desc = "Signature Help" })
 
-map({ "n" }, "K", "O<Esc>j", { desc = "Insert newline above" })
-map({ "n" }, "J", "o<Esc>k", { desc = "Insert newline below" })
-map({ "n" }, "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", { desc = "Hover" })
-map({ "n" }, "gH", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { desc = "Signature Help" })
-
-map("n", "<c-a>", "<nop>")
-map("n", "<c-x>", "<nop>")
-map("n", "<c-a>", "<nop>")
-map("n", "<c-x>", "<nop>")
+-- Buffers
+map("n", "<C-n>", "<cmd>BufferLineCycleNext<CR>", { desc = "Next Buffer" })
+map("n", "<C-p>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Previous Buffer" })
+map("n", "<S-l>", "<cmd>BufferLineMoveNext<CR>", { desc = "Move Buffer Right" })
+map("n", "<S-h>", "<cmd>BufferLineMovePrev<CR>", { desc = "Move Buffer Left" })
 
 -- Increment/Decrement
 map("n", "<C-a>", "<Nop>", { noremap = true, silent = true })
 map("n", "<C-x>", "<Nop>", { noremap = true, silent = true })
-
 map("n", "=", "<C-a>", { noremap = true, silent = true })
 map("n", "-", "<C-x>", { noremap = true, silent = true })
