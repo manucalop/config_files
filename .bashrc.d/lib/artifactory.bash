71a1aab21ab2a28086dd7de73a71a8612cf651df
Manuel Castillo-Lopez <manuel.castillolopez.ext@adevinta.com>
2025-08-18 09:05:56 +0200

    Removed artifactory config

--- .bashrc.d/lib/artifactory.bash
+++
@@ -1,5 +0,0 @@
-export ARTIFACTORY_CONTEXT=https://artifactory.mpi-internal.com/artifactory
-export ARTIFACTORY_USER=manuel.castillolopez.ext@adevinta.com
-
-export UV_INDEX_ARTIFACTORY_USERNAME=$ARTIFACTORY_USER
-export UV_INDEX_ARTIFACTORY_PASSWORD=$ARTIFACTORY_PWD
